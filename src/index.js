808cde782659297475873b3d490c487a27310f0b
Mehulcoder <mehul355180@gmail.com>
2020-04-04 14:09:46 +0530

    Setup client side

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var request = require('request');
+var socketio = require('socket.io');
+var http = require('http');
 var path = require('path');
-var port = 3000 || process.env.PORT;
+
+// ────────────────────────────────────────────────────────────────────────────────
 
 var app = express();
-var publicDirectoryPath = path.join(__dirname,'../public');
+//Manually create server for support in socketio
+var server = http.createServer(app);
+var io = socketio(server);
 
+var port = 3000 || process.env.PORT;
+
+var publicDirectoryPath = path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
 
-app.get('/',(req, res) => {
-    res.render('index');
-})
+//
+// ─── ON CONNECTION ──────────────────────────────────────────────────────────────
+//
 
-app.listen(port, () => {
+io.on('connection', ()=>{
+    console.log('a user connected');
+});
+
+
+//
+// ─── LISTEN TO HTTP SERVER ──────────────────────────────────────────────────────
+//
+
+server.listen(port, () => {
     console.log("Server is listening to port "+port);
 })
